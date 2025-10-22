c7eebcf639fc07d39c7a7ac27f98636e1c81c389
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-22 15:37:28 +0200

    snack 3 using for

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,13 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+
+//Using for
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i] ++;
+    
+}
+console.log(numbers);
+
+
 // Risultato: [3, 9, 5, 8, 3, 88]
