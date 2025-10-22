0aae4b6fe51538f28765cbc73c2b929236bc67d1
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-22 15:57:26 +0200

    sanck 3 using forEach and fix for solution

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,16 +2,24 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 
+incrementedNumbers = []
 //Using for
-// for (let i = 0; i < numbers.length; i++) {
-//     const number = numbers[i] ++;
-    
-// }
-// console.log(numbers);
-
-//using map
-const incrementedNumbers = numbers.map(number => number + 1);
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i] + 1;
+    incrementedNumbers.push(number)
+}
 console.log(incrementedNumbers);
 
+//using map
+const incrementedNumbersMap = numbers.map(number => number + 1);
+console.log(incrementedNumbersMap);
+
+//Using forEach
+const incrementedNumbersEach = [] 
+numbers.forEach(number => {
+    number ++
+    incrementedNumbersEach.push(number)
+});
+console.log(incrementedNumbersEach);
 
 // Risultato: [3, 9, 5, 8, 3, 88]
