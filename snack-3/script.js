8f3c577ff730946d31fcb06808bcfb4d0d06b615
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-22 15:52:11 +0200

    snack 3 using map

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,11 +3,15 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 
 //Using for
-for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i] ++;
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i] ++;
     
-}
-console.log(numbers);
+// }
+// console.log(numbers);
+
+//using map
+const incrementedNumbers = numbers.map(number => number + 1);
+console.log(incrementedNumbers);
 
 
 // Risultato: [3, 9, 5, 8, 3, 88]
